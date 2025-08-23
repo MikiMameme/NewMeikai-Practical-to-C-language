1bc45447c0a2bff5837c0d571fb40b2190be2fd2
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-23 20:00:38 +0900

    01-01_01-01_見えないエラー
    関数形式マクロmax2を利用するプログラム（見えにくいエラーが潜む、正しく実行できない）

---
+++ 01-01_01-01_見えないエラー/01-01_01-01_見えないエラー.cpp
@@ -0,0 +1,18 @@
+﻿/*関数形式マクロmax2を利用するプログラム（見えにくいエラーが潜む、正しく実行できない）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include "max2X1.h"
+
+int main(void)
+{
+	int x, y;
+
+	printf("xの値は :"); scanf("%d", &x);
+	printf("yの値は :"); scanf("%d", &y);
+
+	printf("max2(x, y) = %d\n", max2(x, y));
+
+	return 0;
+}
