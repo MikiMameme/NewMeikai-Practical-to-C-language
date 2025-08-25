d9c33f76c7e5bece90d35ec451a6c73ba947e085
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-25 20:30:41 +0900

    01-02_01-19_配列の初期化２
    初期化子が与えられてない要素が0で初期化されることを確認

---
+++ 01-02_01-19_配列の初期化２/01-02_01-19_配列の初期化２.cpp
@@ -0,0 +1,17 @@
+﻿/*初期化子が与えられてない要素が0で初期化されることを確認*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i;
+	int b[3] = { 1 };
+
+	if (b[1] != 0 || b[2] != 0)
+		puts("正しく初期化されていません");
+	else
+		for (i = 0; i < 3; i++)
+			printf("b[%d] = %d\n", i, b[i]);
+
+	return 0;
+}
