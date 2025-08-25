4c2b7c5287a0366c1e1473dfbf98d1ab75e6f188
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-25 20:22:10 +0900

    01-02_01-18_配列の初期化
    自動記憶域期間をもつ配列を初期化

---
+++ 01-02_01-18_配列の初期化/01-02_01-18_配列の初期化.cpp
@@ -0,0 +1,22 @@
+﻿/*自動記憶域期間をもつ配列を初期化*/
+
+#include<stdio.h>
+
+int vx[3] = { 1, 2, 3 };
+
+int main(void)
+{
+	int i;
+
+	/*自動：古いCでは不可*/
+	int ma[3] = { 1, 2, 3 };
+
+	/*静的：古いCでも可*/
+	static int ms[3] = { 1, 2, 3 };
+
+	for (i = 0; i < 3; i++)
+		printf("vx[%d] = %d ma[%d] = %d  ms[%d] = %d\n",
+			i, vx[i], i,  ma[i], i, ms[i]);
+
+	return 0;
+}
