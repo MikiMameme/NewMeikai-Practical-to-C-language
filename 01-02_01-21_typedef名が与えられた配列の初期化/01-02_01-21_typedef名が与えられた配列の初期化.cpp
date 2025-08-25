a4133b401011f75e396b1a3c9a424a90e9531c4b
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-25 20:59:05 +0900

    01-02_01-21_typedef名が与えられた配列の初期化
    typedef名による配列の初期化を確認

---
+++ 01-02_01-21_typedef名が与えられた配列の初期化/01-02_01-21_typedef名が与えられた配列の初期化.cpp
@@ -0,0 +1,15 @@
+﻿/*typedef名による配列の初期化を確認*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i;
+	typedef int Int5ary[5];
+	Int5ary x = { 1, 2, 3 };
+
+	for (i = 0; i < 5; i++)
+		printf("x[%d] = %d\n", i, x[i]);
+
+	return 0;
+}
