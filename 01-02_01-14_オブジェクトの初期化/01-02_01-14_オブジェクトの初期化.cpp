09987e1dfd023a68ac77cf31b94f5bdbfae274f5
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-25 19:42:47 +0900

    01-02_01-14_オブジェクトの初期化
    初期化子の与えられてない自動記憶域期間をもつオブジェクトが
    不定値、すなわちゴミの値で初期化されることを確認する。
    （実行時チェックで止まる）

---
+++ 01-02_01-14_オブジェクトの初期化/01-02_01-14_オブジェクトの初期化.cpp
@@ -0,0 +1,16 @@
+﻿/*初期化子の与えられてない自動記憶域期間をもつオブジェクトが
+不定値、すなわちゴミの値で初期化されることを確認する*/
+
+/*警告を無効化*/
+#pragma warning(disable: 4700)
+
+#include<stdio.h>
+
+int main(void)
+{
+	int x;
+
+	printf("x = %d\n", x);
+	
+	return 0;
+}
