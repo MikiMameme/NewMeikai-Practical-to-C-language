a682b02d57befae5bf1f4b861f4853540ed380af
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-26 22:11:00 +0900

    02-02_02-05_汎整数拡張
    unsigned charとintの演算

---
+++ 02-02_02-05_汎整数拡張/02-02_02-05_汎整数拡張.cpp
@@ -0,0 +1,22 @@
+﻿/*unsigned charとintの演算*/
+
+#include<stdio.h>
+#include<limits.h>
+
+int main(void)
+{
+	int x;
+	unsigned char ch = UCHAR_MAX;
+
+	printf("chの値は%dです\n", ch);
+	x = ch + 1;
+
+	printf("\nx = ch + 1;\n");
+	printf("xの値は%dです\n", x);
+	x = ++ch;
+
+	printf("nx = ++ch;\n");
+	printf("xの値は%dです\n", x);
+
+	return 0;
+}
