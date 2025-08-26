7f7c10f8d8a28636dc6bec0ab7761defa717b714
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-26 22:30:16 +0900

    02-02_02-06_汎整数拡張2
    signed charとintの演算

---
+++ 02-02_02-06_汎整数拡張2/02-02_02-06_汎整数拡張2.cpp
@@ -0,0 +1,23 @@
+﻿/*signed charとintの演算*/
+
+#include<stdio.h>
+#include<limits.h>
+
+int main(void)
+{
+	int x;
+	signed char ch = CHAR_MAX;
+
+	printf("chの値は%dです\n", ch);
+	x = ch + 1;
+
+	printf("\nx = ch + 1;\n");
+	printf("xの値は%dです\n", x);
+	x = ++ch;
+
+	printf("\nx = ++ch;\n");
+	printf("xの値は%dです\n", x);
+
+	return 0;
+
+}
