f6f46349b2aa6a660ecdeadd62f1fb70fab81e2d
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-26 15:29:57 +0900

    02-01_02-03_if文の制御式
    単純な制御式をもつif文の利用例

---
+++ 02-01_02-03_if文の制御式/02-01_02-03_if文の制御式.cpp
@@ -0,0 +1,20 @@
+﻿/*単純な制御式をもつif文の利用例*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main(void)
+{
+	int nx;
+
+	printf("nxの値を入力せよ :");
+	scanf("%d", &nx);
+
+	if (nx)
+		puts("\aそれは0ではありません");
+	else
+		puts("それは0です");
+
+	return 0;
+}
