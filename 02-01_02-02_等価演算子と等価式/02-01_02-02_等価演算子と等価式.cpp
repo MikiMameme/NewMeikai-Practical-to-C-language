41403f709e41d48619ab71460b349204b5be1dc2
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-26 15:22:38 +0900

    02-01_02-02_等価演算子と等価式
    関係式および等価式と評価した値を表示

---
+++ 02-01_02-02_等価演算子と等価式/02-01_02-02_等価演算子と等価式.cpp
@@ -0,0 +1,22 @@
+﻿/*関係式および等価式と評価した値を表示*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main(void)
+{
+	int nx, ny;
+
+	printf("nxの値: "); scanf("%d", &nx);
+	printf("nyの値: "); scanf("%d", &ny);
+
+	printf("nx < ny  : %d\n", nx < ny);
+	printf("nx <= ny : %d\n", nx <= ny);
+	printf("nx > ny  : %d\n", nx > ny);
+	printf("nx >= ny : %d\n", nx >= ny);
+	printf("nx == ny : %d\n", nx == ny);
+	printf("nx != ny : %d\n", nx != ny);
+
+	return 0;
+}
