e68a24f12b14406ca600a48d07b395f5dab2e4a6
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-27 18:23:28 +0900

    03-01_03-04_ポインタと間接演算子
    ポインタを通じてオブジェクトの値を間接的に変更

---
+++ 03-01_03-04_ポインタと間接演算子/03-01_03-04_ポインタと間接演算子.cpp
@@ -0,0 +1,26 @@
+﻿/*ポインタを通じてオブジェクトの値を間接的に変更*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	int x = 5;
+	int y = 3;
+	int *ptr;
+
+	ptr = &x;
+	printf("x    = %d\n", x);
+	printf("y    = %d\n", y);
+	printf("ptr  = %p\n", ptr);
+	printf("*ptr = %d\n\n", *ptr);
+
+	ptr = &y;
+	*ptr = 4;
+
+	printf("x    = %d\n", x);
+	printf("y    = %d\n", y);
+	printf("ptr  = %p\n", ptr);
+	printf("*ptr = %d\n", *ptr);
+
+	return 0;
+}
