40ddacbd24b47a496e635ecba706c20510d560ff
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-27 19:51:12 +0900

    03-02_03-06_関数にポインタを渡す
    サークルの人数の増減

---
+++ 03-02_03-06_関数にポインタを渡す/03-02_03-06_関数にポインタを渡す.cpp
@@ -0,0 +1,29 @@
+﻿/*サークルの人数の増減*/
+
+#include<stdio.h>
+
+/*人数増加*/
+void increment(int *no)
+{
+	++*no;
+}
+
+/*人数減少*/
+void decrement(int *no)
+{
+	--*no;
+}
+
+int main(void)
+{
+	int x = 5;
+	int y = 3;
+
+	increment(&x);
+	decrement(&y);
+
+	printf("サークルxの所属人数 = %d\n", x);
+	printf("サークルyの所属人数 = %d\n", y);
+
+	return 0;
+}
