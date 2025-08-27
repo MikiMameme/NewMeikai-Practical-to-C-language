c2edb14f81eb5a20b4ba87d29d8806b8f1964932
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-27 16:45:36 +0900

    02-02_02-08_問題の解決2
    -1をunsigned int型に変換した結果がUINT_MAXになることを確認

---
+++ 02-02_02-08_問題の解決2/02-02_02-08_問題の解決2.cpp
@@ -0,0 +1,12 @@
+﻿/*-1をunsigned int型に変換した結果がUINT_MAXになることを確認*/
+
+#include<stdio.h>
+#include<limits.h>
+
+int main(void)
+{
+	printf("(unsigned)-1 = %u\n", (unsigned)-1);
+	printf("UINT_MAX     = %u\n", UINT_MAX);
+
+	return 0;
+}
