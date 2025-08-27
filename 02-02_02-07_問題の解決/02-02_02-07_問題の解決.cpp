78ad814fdda962b12f1ffb5d2edc409c507731a4
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-27 16:35:36 +0900

    02-02_02-07_問題の解決
    符号付き整数と符号なし整数の比較

---
+++ 02-02_02-07_問題の解決/02-02_02-07_問題の解決.cpp
@@ -0,0 +1,23 @@
+﻿/*符号付き整数と符号なし整数の比較*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	int sdata = -1;
+	unsigned udata = 1;
+
+	printf("sdata < udata すなわち -1 < 1U は ");
+	if (sdata < udata)
+		puts("真");
+	else
+		puts("偽");
+
+	printf("(unsigned)sdata < udata すなわち (unsigned)-1 < 1Uは ");
+	if ((unsigned)sdata < udata)
+		puts("真");
+	else
+		puts("偽");
+
+	return 0;
+}
