4535f91b46c0928d3582c0b01a9c34668c2b4e91
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-29 22:29:19 +0900

    03-03_03-12_配列の終端を表す番兵2
    文字列を表示する

---
+++ 03-03_03-12_配列の終端を表す番兵2/03-03_03-12_配列の終端を表す番兵2.cpp
@@ -0,0 +1,26 @@
+﻿/*文字列を表示する*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+/*文字列を1文字ずつ走査して表示*/
+void putstr(const char s[])
+{
+	int i;
+	for (i = 0; s[i] != '\0'; i++)
+		putchar(s[i]);
+}
+
+int main(void)
+{
+	char str[128];
+
+	printf("文字列を入力せよ:");
+	scanf("%s", str);
+
+	putstr(str);
+	putchar('\n');
+
+	return 0;
+}
