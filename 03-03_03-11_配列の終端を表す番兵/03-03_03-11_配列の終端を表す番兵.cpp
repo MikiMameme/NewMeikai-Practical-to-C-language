019fd8f7139bd4c6460cfbbe59b857dc04f4b128
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-29 22:20:04 +0900

    03-03_03-11_配列の終端を表す番兵
    配列の全要素を合計する（要素数をやり取りしない）

---
+++ 03-03_03-11_配列の終端を表す番兵/03-03_03-11_配列の終端を表す番兵.cpp
@@ -0,0 +1,38 @@
+﻿/*配列の全要素を合計する（要素数をやり取りしない）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+#define INVALID -1
+
+/*ばんお英INVALIDより前の要素を合計*/
+int sumup(int v[])
+{
+	int i;
+	int sum = 0;
+
+	for(i = 0; v[i] != INVALID; i++)
+		sum += v[i];
+	return sum;
+}
+
+int main(void)
+{
+	int i;
+	int a[128];
+	int na = sizeof(a) / sizeof(a[0]);
+
+	printf("%d個の非負の整数を入力せよ（終了は-1）\n", na - 1);
+	for (i = 0; i < na - 1; i++) {
+		printf("a[%d]:", i);
+		scanf("%d", &a[i]);
+		if (a[i] == INVALID) break;
+	}
+	if (i == na - 1)
+		a[i] = INVALID;
+
+	printf("合計 = %d\n", sumup(a));
+
+	return 0;
+}
