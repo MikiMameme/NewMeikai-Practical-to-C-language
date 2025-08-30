54dd0474d2a4c9af72229d3087a81e82eac1c5f2
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-30 18:42:13 +0900

    03-05_03-12_単一オブジェクトの生成2
    動的に生成した整数に値を読み込む(間違い)
    VisualStudioではデバッガが例外で止まる。

---
+++ 03-05_03-12_単一オブジェクトの生成2/03-05_03-12_単一オブジェクトの生成2.cpp
@@ -0,0 +1,27 @@
+﻿/*動的に生成した整数に値を読み込む(間違い)*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int* x;
+
+	x = (int*)calloc(1, sizeof(int));
+
+	if (x == NULL)
+		puts("記憶域の確保に失敗しました");
+	else{
+		printf("整数値を入力してください :");
+		/*おかしい？*/
+		scanf("%d", &x);
+		/*処理系によっては動作しない*/
+		printf("あなたは%dと入力しましたね\n", *x);
+		free(x);
+
+	}
+
+	return 0;
+}
