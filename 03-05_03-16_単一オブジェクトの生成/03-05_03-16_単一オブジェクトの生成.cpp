805b77e7a5cc31cae889a52acc5e7dbd1dc94b3c
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-30 18:33:22 +0900

    03-05_03-16_単一オブジェクトの生成
    動的に生成した整数に値を代入して表示

---
+++ 03-05_03-16_単一オブジェクトの生成/03-05_03-16_単一オブジェクトの生成.cpp
@@ -0,0 +1,19 @@
+﻿/*動的に生成した整数に値を代入して表示*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int *x;
+	x =(int*) calloc(1, sizeof(int));
+	if (x == NULL)
+		puts("記憶域の確保に失敗しました");
+	else {
+		*x = 123;
+		printf("*x = %d\n", *x);
+		free(x);
+	}
+
+	return 0;
+}
