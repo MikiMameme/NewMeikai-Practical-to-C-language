72ba8d40f209c1a5411cda797fb26c338f126ed9
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-30 20:25:32 +0900

    03-05_03-19_多次元配列オブジェクトの生成
    2次元配列を動的に生成

---
+++ 03-05_03-19_多次元配列オブジェクトの生成/03-05_03-19_多次元配列オブジェクトの生成.cpp
@@ -0,0 +1,31 @@
+﻿/*2次元配列を動的に生成*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int (*x)[3];
+	int n;
+
+	puts("n x 3の2次元配列を確保します");
+	printf("nの値は :");
+	scanf("%d", &n);
+
+	x =(int (*)[3])calloc(n * 3, sizeof(int));
+
+	if (x == NULL)
+		puts("記憶域の確保に失敗しました");
+	else {
+		int i, j;
+
+		for (i = 0; i < n; i++)
+			for (j = 0; j < 3; j++)
+				printf("x[%d][%d] = %d\n", i, j, x[i][j]);
+		free(x);
+	}
+
+	return 0;
+}
