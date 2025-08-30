3fd3583ef86bba19be5ffe6cebfdce11e6348f89
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-30 20:12:33 +0900

    03-05_03-18_配列オブジェクトの生成
    整数配列を動的に生成

---
+++ 03-05_03-18_配列オブジェクトの生成/03-05_03-18_配列オブジェクトの生成.cpp
@@ -0,0 +1,32 @@
+﻿/*整数配列を動的に生成*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int* x;
+	int i, nx;
+
+	printf("生成する配列の要素数 :");
+	scanf("%d", &nx);
+
+	x =(int*) calloc(nx, sizeof(int));
+
+	if (x == NULL)
+		puts("記憶域の確保に失敗しました");
+	else {
+		for (i = 0; i < nx; i++)
+			x[i] = i;
+
+		for (i = 0; i < nx; i++)
+			printf("x[%d] = %d\n", i, x[i]);
+
+		free(x);
+	}
+
+	return 0;
+}
