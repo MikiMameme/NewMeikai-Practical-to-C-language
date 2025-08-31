26dcce6ccf2e401759a6d8eed0b6ba5f5c5804a6
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-31 17:52:49 +0900

    04-01_04-06_アルファベットの規則性
    文字定数の大きさを表示

---
+++ 04-01_04-06_アルファベットの規則性/04-01_04-06_アルファベットの規則性.cpp
@@ -0,0 +1,12 @@
+﻿/*文字定数の大きさを表示*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("sizeof(char) = %u\n", (unsigned)sizeof(char));
+	printf("sizeof(int) = %u\n", (unsigned)sizeof(int));
+	printf("sizeof('A') = %u\n", (unsigned)sizeof('A'));
+
+	return 0;
+}
