8dddef052a109f621d8ddb9e6f8cbf7de7baffea
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-31 18:50:10 +0900

    04-01_04-10_文字列リテラルの型と値
    文字列リテラルを評価した値を表示

---
+++ 04-01_04-10_文字列リテラルの型と値/04-01_04-10_文字列リテラルの型と値.cpp
@@ -0,0 +1,14 @@
+﻿/*文字列リテラルを評価した値を表示*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	const char* ptr = "ABCD";
+
+	printf("ptr = %s\n", ptr);
+	printf("ptr = %p\n", ptr);
+	printf("*ptr = %c\n", *ptr);
+
+	return 0;
+}
