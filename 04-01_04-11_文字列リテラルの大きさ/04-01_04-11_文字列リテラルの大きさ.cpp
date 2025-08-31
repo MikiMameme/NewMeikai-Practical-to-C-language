4015b4497e1fe6808ce2e192244910212b276819
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-31 18:56:08 +0900

    04-01_04-11_文字列リテラルの大きさ
    文字列リテラルの大きさを表示

---
+++ 04-01_04-11_文字列リテラルの大きさ/04-01_04-11_文字列リテラルの大きさ.cpp
@@ -0,0 +1,11 @@
+﻿/*文字列リテラルの大きさを表示*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("sizeof(\"ABC\") = %u\n", (unsigned)sizeof("ABC"));
+	printf("sizeof(\"UVW\\0XYZ\") = %u\n", (unsigned)sizeof("UVW\0XYZ"));
+
+	return 0;
+}
