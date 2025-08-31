c1ade5b7fd7a62c97622a61468947ef2035077c3
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-31 18:18:29 +0900

    04-01_04-07_文字列
    文字列の初期化と表示（文字列は最初のナル文字であることを確認）

---
+++ 04-01_04-07_文字列/04-01_04-07_文字列.cpp
@@ -0,0 +1,14 @@
+﻿/*文字列の初期化と表示（文字列は最初のナル文字であることを確認）*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	char a[4] = { 'S', 'X', '\0', '2' };
+	char b[4] = "ABC";
+
+	printf("a = %s\n", a);
+	printf("b = %s\n", b);
+
+	return 0;
+}
