dde3e81c1144bfc386c573278a9730696c9cf3c7
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-31 20:35:47 +0900

    04-01_04-12_文字列リテラルと文字列定数
    同じ綴りの文字列リテラル

---
+++ 04-01_04-12_文字列リテラルと文字列定数/04-01_04-12_文字列リテラルと文字列定数.cpp
@@ -0,0 +1,16 @@
+﻿/*同じ綴りの文字列リテラル*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	char s1[] = "ABC";
+	char s2[] = "ABC";
+
+	s1[0] = 'Z';
+
+	printf("s1 = %s\n", s1);
+	printf("s2 = %s\n", s2);
+
+	return 0;
+}
