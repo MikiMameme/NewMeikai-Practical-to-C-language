9e3b0d633a79be6c20ea918c7375bb5b601d184c
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-31 20:52:30 +0900

    04-01_04-13_ポインタによる文字列の取り扱い
    《配列による文字列》と《ポインタによる文字列》

---
+++ 04-01_04-13_ポインタによる文字列の取り扱い/04-01_04-13_ポインタによる文字列の取り扱い.cpp
@@ -0,0 +1,14 @@
+﻿/*《配列による文字列》と《ポインタによる文字列》*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	char ary[] = "ABC";
+	const char *ptr = "XYZ";
+
+	printf("ary = %s\n", ary);
+	printf("ptr = %s\n", ptr);
+
+	return 0;
+}
