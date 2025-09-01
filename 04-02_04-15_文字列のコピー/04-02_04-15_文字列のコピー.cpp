3a33fb0bdf1dc7d4aa92eb4f83030e1a612a2b23
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-01 19:15:24 +0900

    04-02_04-15_文字列のコピー
    文字列コピーを行う

---
+++ 04-02_04-15_文字列のコピー/04-02_04-15_文字列のコピー.cpp
@@ -0,0 +1,38 @@
+﻿/*文字列コピーを行う*/
+
+#include<stdio.h>
+
+/*文字列fをtにコピー*/
+void scp1(char* f, char* t)
+{
+	while (*f != '\0') {
+		*t = *f;
+		f++; t++;
+	}
+	*t = '\0';
+}
+
+
+/*文字列fをtにコピー（バグあり）*/
+void scp2(char* f, char* t)
+{
+	do {
+		*t++ = *f++;
+	} while (*f);
+}
+
+int main(void)
+{
+	char astr[7] = "ABC";
+	char bstr[7] = "XXXXXX";
+	char cstr[7] = "YYYYYY";
+
+	scp1(astr, bstr);
+	scp2(astr, cstr);
+
+	printf("astr = %s\n", astr);
+	printf("bstr = %s\n", bstr);
+	printf("cstr = %s\n", cstr);
+
+	return 0;
+}
