57d0c9726c71815b230110800ca9147ddd99c194
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-02 00:31:00 +0900

    04-02_04-19_空文字列化の２つの方法２
    strcpy関数によって文字列を空にする

---
+++ 04-02_04-19_空文字列化の２つの方法２/04-02_04-19_空文字列化の２つの方法２.cpp
@@ -0,0 +1,17 @@
+﻿/*strcpy関数によって文字列を空にする*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+	char str[4] = "ABC";
+
+	strcpy(str, " ");
+
+	printf("str = \"%s\"\n", str);
+
+	return 0;
+}
