8c016e31da157d22285dcf9215cf10e59178c295
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-01 20:25:35 +0900

    04-02_04-16_文字列のコピー2
    文字列コピーを行う関数の実現例(関数のみ)

---
+++ 04-02_04-16_文字列のコピー2/04-02_04-16_文字列のコピー2.cpp
@@ -0,0 +1,47 @@
+﻿/*文字列コピーを行う関数の実現例(関数のみ)*/
+
+/*文字列をコピー１*/
+char* scpy1(char* d, const char* s)
+{
+	int i = 0;
+
+	while((d[i] = s[i]) != '\0')
+		i++;
+
+	return d;
+}
+
+/*文字列をコピー２*/
+char* scpy2(char* d, const char* s)
+{
+	char* p = d;
+
+	while ((*d = *s) != '\0') {
+		d++;
+		s++;
+	}
+
+	return p;
+}
+
+/*文字列をコピー３*/
+char* scpy3(char* d, const char* s)
+{
+	char* p = d;
+
+	while ((*d++ = *s++) != '\0')
+		;
+
+	return p;
+}
+
+/*文字列をコピー４*/
+char* scpy4(char* d, const char* s)
+{
+	char* p = d;
+
+	while (*d++ = *s++)
+		;
+
+	return p;
+}
