bbac5cdf8284a0ca8d885b467209f89f8e217ab9
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-02 17:50:25 +0900

    04-02_04-24_記憶域のクリア3
    文字列strにナル文字をno個埋める（修正版）

---
+++ 04-02_04-24_記憶域のクリア3/04-02_04-24_記憶域のクリア3.cpp
@@ -0,0 +1,23 @@
+﻿/*文字列strにナル文字をno個埋める（修正版）*/
+
+#include<stdio.h>
+#include<string.h>
+
+void clear_string(char* str, int no)
+{
+	memset(str, '\0', no);
+}
+
+int main(void)
+{
+	int i;
+	char s[10] = "ABCDEFGHI";
+
+	clear_string(s, sizeof(s));
+
+	for (i = 0; i < sizeof(s); i++)
+
+		printf("s[%d] = %d\n", i, s[i]);
+
+	return 0;
+}
