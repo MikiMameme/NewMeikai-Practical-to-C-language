a6304ad8d82981e0465e81a723b0dcd7256da2ce
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-02 20:02:08 +0900

    04-02_04-25_文字列の自動生成
    読み込んだ文字列の複製を動的に生成する

---
+++ 04-02_04-25_文字列の自動生成/04-02_04-25_文字列の自動生成.cpp
@@ -0,0 +1,27 @@
+﻿/*読み込んだ文字列の複製を動的に生成する*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(void)
+{
+	char st[128];
+	char *pt;
+
+	printf("文字列strを入力してください");
+	scanf("%s", st);
+
+	pt = (char*)malloc(strlen(st) + 1);
+
+	if (pt) {
+		strcpy(pt, st);
+		printf("st = %s\n", st);
+		printf("pt = %s\n", pt);
+		free(pt);
+	}
+
+	return 0;
+}
