ff5d72510c7efe29429afee83469e0b7575ca514
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-02 17:10:29 +0900

    04-02_04-21_問題の解決
    単語をファイルから読み込んで表示する（改良版）

---
+++ 04-02_04-21_問題の解決/04-02_04-21_問題の解決.cpp
@@ -0,0 +1,56 @@
+﻿/*単語をファイルから読み込んで表示する（改良版）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<ctype.h>
+
+#define Q_NO 3
+
+FILE* fp;
+
+/*初期処理*/
+int initialize(void)
+{
+	fp = fopen("DATA", "r");
+	return(fp == NULL) ? 0 : 1;
+}
+
+/*終了処理*/
+void ending(void)
+{
+	fclose(fp);
+}
+
+/*メイン関数*/
+int main(void)
+{
+	if (initialize()) {
+		int q, ch;
+		char qus[20];
+		char ans[20];
+
+		for (q = 0; q < Q_NO; q++) {
+			int i;
+
+			if ((ch = fgetc(fp)) == EOF) goto ending;
+
+			for (i = 0; !(isspace(ch)); i++) {
+				qus[i] = ch;
+				ch = fgetc(fp);
+			}
+			qus[i] = '\0';
+
+			ch = fgetc(fp);
+			for (i = 0; !(isspace(ch)); i++){
+				ans[i] = ch;
+				ch = fgetc(fp);
+			}
+			ans[i] = '\0';
+			printf("問題=%s 解答=%s\n", qus, ans);
+		}
+	ending:
+		ending();
+	}
+	return 0;
+}
