6379b7006e534fcffaf46a20029c7bac14c7a1af
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-02 17:20:11 +0900

    04-02_04-22_記憶域のクリア
    文字列にナル文字を埋める

---
+++ 04-02_04-22_記憶域のクリア/04-02_04-22_記憶域のクリア.cpp
@@ -0,0 +1,17 @@
+﻿/*文字列にナル文字を埋める*/
+
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+	int i;
+	char s[10] = "ACBDEFGHI";
+
+	memset(s, '\0', sizeof(s));
+
+	for (i = 0; i < sizeof(s); i++)
+		printf("s[%d] = %d\n", i, s[i]);
+
+	return 0;
+}
