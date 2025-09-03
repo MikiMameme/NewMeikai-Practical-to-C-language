2cfeaa8aa367647e7cb5adb7a27b660eebb339d7
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-03 22:02:17 +0900

    06-01_06-03_宣言されてない関数の呼び出し
    pow関数を呼び出して3乗値を求める（コンパイルエラーまたは正しい結果が得られない）

---
+++ 06-01_06-03_宣言されてない関数の呼び出し/06-01_06-03_宣言されてない関数の呼び出し.cpp
@@ -0,0 +1,17 @@
+﻿/*pow関数を呼び出して3乗値を求める（コンパイルエラーまたは正しい結果が得られない）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main(void)
+{
+	double x;
+
+	printf("実数値を入力せよ :");
+	scanf("%lf", &x);
+													/*型の不一致で暴走*/
+	printf("その数の3乗は%.3fです\n", pow(x, 3.0)); /*Visual Studio → コンパイルエラーで止まる*/
+
+	return 0;
+}
