bd74e83a2cfb852853d8ff7d0471195d47a3bb64
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-03 19:12:01 +0900

    05-01_05-05_空文
    文字列のコピー（空文の利用、繰り返し文の本体として）

---
+++ 05-01_05-05_空文/05-01_05-05_空文.cpp
@@ -0,0 +1,25 @@
+﻿/*文字列のコピー（空文の利用、繰り返し文の本体として）*/
+
+#include<stdio.h>
+
+/*文字列sをdにコピー*/
+char* strcopy(char* d, const char* s)
+{
+	char* p = d;
+	while (*d++ = *s++)
+		;
+	return p;
+}
+
+int main(void)
+{
+	char sx[20] = "ABC";
+	char sy[20] = " ";
+
+	strcopy(sy, sx);
+
+	printf("文字列sx = %s\n", sx);
+	printf("文字列sy = %s\n", sy);
+
+	return 0;
+}
