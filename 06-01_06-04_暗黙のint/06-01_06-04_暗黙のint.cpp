7f4398079a7625ae89cb20c09a330fc6f347f88e
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-03 22:24:00 +0900

    06-01_06-04_暗黙のint
    C89までは「暗黙のint型宣言」が許されてたが、C99以降は廃止された

---
+++ 06-01_06-04_暗黙のint/06-01_06-04_暗黙のint.cpp
@@ -0,0 +1,29 @@
+﻿/*型が与えられずに宣言されたものがint型になることを確認*/
+
+#include<stdio.h>
+
+func(a, b) /*C89までは「暗黙のint型宣言」が許されてたが、C99以降は廃止された*/
+{			/*つまりこの書き方は許されないことに注意*/
+	struct {
+		x;
+		y;
+	}z;
+
+	z.x = a;
+	z.y = b;
+
+	printf("z.x = %d\n", z.x);
+	printf("z.y = %d\n", z.y);
+
+	return z.x + z.y;
+}
+
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+
+	printf("func(a, b) = %d\n", func(a, b));
+
+	return 0;
+}
