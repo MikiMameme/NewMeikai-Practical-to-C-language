5df77a0502999d235ee44f7ff76a405e49c7a572
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-03 21:19:19 +0900

    06-01_06-02_関数定義の順序2
    2乗値を求める（関数sqrを末尾側に配置、コンパイルエラーになる）

---
+++ 06-01_06-02_関数定義の順序2/06-01_06-02_関数定義の順序2.cpp
@@ -0,0 +1,22 @@
+﻿/*2乗値を求める（関数sqrを末尾側に配置、コンパイルエラーになる）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main(void)
+{
+	double x;
+
+	printf("実数値を入力せよ :");
+	scanf("%lf", &x);
+
+	printf("その数の2乗は%.3fです\n", sqr(x));
+
+	return 0;
+}
+
+double sqr(double x)
+{
+	return x * x;
+}
