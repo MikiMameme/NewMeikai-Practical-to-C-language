3d1769fd278b9cabdc7d5270b8f2eb1e7b18479a
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-04 19:56:10 +0900

    06-02_06-08_関数原型宣言２
    3乗値を求める（標準ヘッダをインクルード）

---
+++ 06-02_06-08_関数原型宣言２/06-02_06-08_関数原型宣言２.cpp
@@ -0,0 +1,18 @@
+﻿/*3乗値を求める（標準ヘッダをインクルード）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<math.h>
+
+int main(void)
+{
+	double x;
+
+	printf("実数値を入力せよ :");
+	scanf("%lf", &x);
+
+	printf("その数の３乗は%.3fです\n", pow(x, 3));
+
+	return 0;
+}
