d75acedc59a1c8b3745488ae32400f603b139af9
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-04 19:05:50 +0900

    06-02_06-07_関数原型宣言
    *3乗値を求める（関数原型宣言）

---
+++ 06-02_06-07_関数原型宣言/06-02_06-07_関数原型宣言.cpp
@@ -0,0 +1,19 @@
+﻿/*3乗値を求める（原型関数宣言）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+double pow(double, double);
+
+int main(void)
+{
+	double x;
+
+	printf("実数値を入力せよ :");
+	scanf("%lf", &x);
+
+	printf("その数の3乗は%.3fです\n", pow(x, 3));
+
+	return 0;
+}
