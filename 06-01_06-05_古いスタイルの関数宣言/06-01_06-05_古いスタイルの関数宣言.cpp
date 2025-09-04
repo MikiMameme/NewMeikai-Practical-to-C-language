4fdd112ca7e84aca47052e133f6da6420d49f4aa
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-04 18:23:44 +0900

    06-01_06-05_古いスタイルの関数宣言
    3乗値を求める（古いスタイル）

---
+++ 06-01_06-05_古いスタイルの関数宣言/06-01_06-05_古いスタイルの関数宣言.cpp
@@ -0,0 +1,19 @@
+﻿/*3乗値を求める（古いスタイル）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+extern double pow();
+
+int main(void)
+{
+	double x;
+
+	printf("実数値を入力せよ :");
+	scanf("%lf", &x);
+
+	printf("その数の3乗は%.3fです\n", pow(x, 3.0));
+
+	return 0;
+}
