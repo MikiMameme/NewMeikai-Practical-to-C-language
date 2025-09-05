4181ff913a373749942bb338f83e876ecac98b1a
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-05 17:51:50 +0900

    07-01_07-04_構造体とマクロ3
    マクロによって構造体に名前を与える（その３：エラー）

---
+++ 07-01_07-04_構造体とマクロ3/07-01_07-04_構造体とマクロ3.cpp
@@ -0,0 +1,17 @@
+﻿/*マクロによって構造体に名前を与える（その３：エラー）*/
+
+struct __comp { double re, im; };
+
+#define complex struct __comp
+#define compptr struct __comp *
+
+int main(void)
+{
+	complex a, b;
+	compptr pa, pb; /* compptr pa, *pb とするべき*/
+
+	pa = &a;
+	pb = &b;
+
+	return 0;
+}
