3a1ea0b4c3fc578a7232bd4509cfa03d7e7334eb
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-05 18:16:45 +0900

    07-01_07-05_構造体とtypedef宣言
    typedef宣言によって構造体に名前を与える（その１）

---
+++ 07-01_07-05_構造体とtypedef宣言/07-01_07-05_構造体とtypedef宣言.cpp
@@ -0,0 +1,17 @@
+﻿/*typedef宣言によって構造体に名前を与える（その１）*/
+
+typedef struct { double re, im; }complex;
+
+int main(void)
+{
+	complex a, b = { 0.0 };
+	complex x, y = { 0.0 };
+	complex* pa, * pb;
+
+	a = b;
+	x = b;
+	pa = &a;
+	pb = &b;
+
+	return 0;
+}
