b1e9788e13750e1f695944f3bf7ecf544c5dc24d
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-05 17:34:51 +0900

    07-01_07-03_構造体とマクロ2
    マクロによって構造体に名前を与える（その２）

---
+++ 07-01_07-03_構造体とマクロ2/07-01_07-03_構造体とマクロ2.cpp
@@ -0,0 +1,16 @@
+﻿/*マクロによって構造体に名前を与える（その２）*/
+
+struct __comp { double re, im; };
+
+#define complex struct __comp
+
+int main(void)
+{
+	complex a, b; /*処理系によってはエラー*/
+	complex x, y;
+
+	a = b;
+	x = b;
+
+	return 0;
+}
