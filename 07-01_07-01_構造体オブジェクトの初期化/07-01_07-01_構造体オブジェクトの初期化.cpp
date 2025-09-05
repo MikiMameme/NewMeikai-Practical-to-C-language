3c82d198fa75fe6658d7ab143b5b4321f7f68fe2
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-05 16:58:42 +0900

    07-01_07-01_構造体オブジェクトの初期化
    構造体オブジェクトの初期化（初期化子不足のメンバは０で初期化される）

---
+++ 07-01_07-01_構造体オブジェクトの初期化/07-01_07-01_構造体オブジェクトの初期化.cpp
@@ -0,0 +1,21 @@
+﻿/*構造体オブジェクトの初期化（初期化子不足のメンバは０で初期化される）*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	struct xy {
+		int x;
+		double y;
+	};
+	struct xy s = { 1 };
+	struct xy t = s;
+
+	printf("s.x = %d\n", s.x);
+	printf("s.y = %f\n", s.y);
+	printf("t.x = %d\n", t.x);
+	printf("t.y = %f\n", t.y);
+
+	return 0;
+
+}
