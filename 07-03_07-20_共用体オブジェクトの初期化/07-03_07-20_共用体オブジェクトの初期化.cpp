6f11eaf055d6d6ee527774f6d3f4bb6fcddfcead
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-06 19:07:52 +0900

    07-03_07-20_共用体オブジェクトの初期化
    共用体の初期化

---
+++ 07-03_07-20_共用体オブジェクトの初期化/07-03_07-20_共用体オブジェクトの初期化.cpp
@@ -0,0 +1,21 @@
+﻿/*共用体の初期化*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	union uxy {
+		int x;
+		double y;
+	};
+
+	union uxy s = { 5 };
+	union uxy t = s;
+
+	printf("s.x = %d\n", s.x);
+	printf("s.y = %f\n", s.y);
+	printf("t.x = %d\n", t.x);
+	printf("t.y = %f\n", t.y);
+
+	return 0;
+}
