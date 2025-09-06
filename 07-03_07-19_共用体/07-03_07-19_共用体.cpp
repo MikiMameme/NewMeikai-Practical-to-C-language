e0aebeb06b56d1e55dc0e96618e78d7c70f34d61
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-06 19:01:21 +0900

    07-03_07-19_共用体
    共用体への値の代入と表示

---
+++ 07-03_07-19_共用体/07-03_07-19_共用体.cpp
@@ -0,0 +1,17 @@
+﻿/*共用体への値の代入と表示*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	union uxy {
+		int x;
+		double y;
+	}u;
+
+	u.x = 1;
+	printf("s.x = %d\n", u.x);
+	printf("s.y = %f\n", u.y);
+
+	return 0;
+}
