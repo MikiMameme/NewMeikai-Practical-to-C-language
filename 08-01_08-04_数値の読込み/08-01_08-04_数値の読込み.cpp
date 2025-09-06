e3c59fa0d53c54470a9daf274e291b0d00937cf9
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-06 21:56:57 +0900

    08-01_08-04_数値の読込み
    数値を読み込んで表示

---
+++ 08-01_08-04_数値の読込み/08-01_08-04_数値の読込み.cpp
@@ -0,0 +1,20 @@
+﻿/*数値を読み込んで表示*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main(void)
+{
+	int num;
+
+	while (1) {
+		printf("整数値を入力せよ\n(9999と入力すると終了します):");
+		scanf("%d", &num);
+		if (num == 9999)break;
+
+		printf("あなたは%dと入力しましたね\n", num);
+	}
+
+	return 0;
+}
