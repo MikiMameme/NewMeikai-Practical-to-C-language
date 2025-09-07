419e5e034321f62d6547094c9cac779e4ff57e4d
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-07 22:29:52 +0900

    08-02_08-08_sprintf関数による文字列への書き出し
    文字列"No.1","No.2"...と作成する

---
+++ 08-02_08-08_sprintf関数による文字列への書き出し/08-02_08-08_sprintf関数による文字列への書き出し.cpp
@@ -0,0 +1,19 @@
+﻿/*文字列"No.1","No.2"...と作成する*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i;
+	char ns[20][6];
+
+	for (i = 0; i < 20; i++)
+		sprintf(ns[i], "No.%02d", i + 1);
+
+	for (i = 0; i < 20; i++)
+		printf("%s\n", ns[i]);
+
+	return 0;
+}
