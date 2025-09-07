980fc64645af0c800b95103cd07cb3f378778e23
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-07 23:21:52 +0900

    09-01_09-01_呼び出した関数が実行されない？
    入力の確認を行う（問題あり：getchar関数の呼び出しが無視される？）

---
+++ 09-01_09-01_呼び出した関数が実行されない？/09-01_09-01_呼び出した関数が実行されない？.cpp
@@ -0,0 +1,23 @@
+﻿/*入力の確認を行う（問題あり：getchar関数の呼び出しが無視される？）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main(void)
+{
+	int ch;
+	char name[20];
+
+	printf("名前を入力してください :");
+	scanf("%s", name);
+
+	printf("よろしいですか(Y/N) :");
+
+	ch = getchar();
+	if (ch == 'Y' || ch == 'y') {
+		printf("こんにちは%sさん\n", name);
+	}
+
+	return 0;
+}
