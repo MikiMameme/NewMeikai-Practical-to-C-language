6febf2ffd5b9e916faccde579c958e120113c24b
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-08 19:25:38 +0900

    09-02_09-09_改行文字の扱い
    バイナリファイルをテキストファイルとして読み取る

---
+++ 09-02_09-09_改行文字の扱い/09-02_09-09_改行文字の扱い.cpp
@@ -0,0 +1,29 @@
+﻿/*バイナリファイルをテキストファイルとして読み取る*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+#define MAX 10
+
+int main(void)
+{
+	FILE* fp;
+	int i, x, y;
+	int a[MAX];
+
+	if ((fp = fopen("TEMP", "r")) != NULL) {
+		fread(a, sizeof(int), MAX, fp);
+		fread(&x, sizeof(int), 1, fp);
+		fread(&y, sizeof(int), 1, fp);
+
+		fclose(fp);
+
+		for (i = 0; i < MAX; i++)
+			printf("a[%d] = %d\n", i, a[i]);
+		printf("x = %d\n", x);
+		printf("y = %d\n", y);
+	}
+
+	return 0;
+}
