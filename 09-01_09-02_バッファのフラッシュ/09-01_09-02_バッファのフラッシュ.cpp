a5f20143e4480d22e74eff61968cf771af168218
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-08 17:48:38 +0900

    09-01_09-02_バッファのフラッシュ
    入力の確認を行う（修正版？）

---
+++ 09-01_09-02_バッファのフラッシュ/09-01_09-02_バッファのフラッシュ.cpp
@@ -0,0 +1,25 @@
+﻿/*入力の確認を行う（修正版？）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main(void)
+{
+	int ch;
+	char name[20];
+
+	printf("名前を入力してください :");
+	scanf("%s", name);
+
+	printf("よろしいですか(Y/N) :");
+
+	fflush(stdin);
+
+	ch = getchar();
+	if (ch == 'Y' || ch == 'y') {
+		printf("こんにちは%sさん\n", name);
+	}
+
+	return 0;
+}
