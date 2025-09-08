e7205acad5639b9a07db711abe8b426175fe5002
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-08 17:51:19 +0900

    09-01_09-03_問題の解決
    入力の確認を行う（修正版１）

---
+++ 09-01_09-03_問題の解決/09-01_09-03_問題の解決.cpp
@@ -0,0 +1,34 @@
+﻿/*入力の確認を行う（修正版１）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<ctype.h>
+
+/*確認用関数*/
+int kakunin(void)
+{
+	int ch;
+
+	while (isspace(ch = getchar()) && ch != EOF)
+		;
+	return ch;
+}
+
+int main(void)
+{
+	int ch;
+	char name[20];
+
+	printf("名前を入力してください :");
+	scanf("%s", name);
+
+	printf("よろしいですか(Y/N) :");
+
+	ch = kakunin();
+	if (ch == 'Y' || ch == 'y') {
+		printf("こんにちは%sさん\n", name);
+	}
+
+	return 0;
+}
