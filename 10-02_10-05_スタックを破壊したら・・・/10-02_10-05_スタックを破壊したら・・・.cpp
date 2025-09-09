b68da53680e9ac71b0e33274da14f95e4766bbc9
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-09 20:18:41 +0900

    10-02_10-05_スタックを破壊したら・・・
    スタックを破壊するプログラム

---
+++ 10-02_10-05_スタックを破壊したら・・・/10-02_10-05_スタックを破壊したら・・・.cpp
@@ -0,0 +1,24 @@
+﻿/*スタックを破壊するプログラム*/
+
+#include<stdio.h>
+
+/*スタックを破壊してしまう関数*/
+void func(void)
+{
+	int i;
+	char x[5];
+
+	puts("関数funcの実行中です");
+	/*範囲外にも書き込み！*/
+	for (i = -2; i < 8; i++)/*VisualStudioでは例外で停止する*/
+		x[i] = 0;/*5個は正常、5個は範囲外への危険な書き込み*/
+}
+
+int main(void)
+{
+	puts("関数funcを呼び出します");
+	func();
+	puts("関数funcから戻ってきました");
+
+	return 0;
+}
