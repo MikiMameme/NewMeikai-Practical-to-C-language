5f641c1e425a59b64ce46be1239902289e508312
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-10 22:48:04 +0900

    11-02_11-04-5_ソースファイルの分割

---
+++ 11-02_11-04-5_ソースファイルの分割/ungetchr.cpp
@@ -0,0 +1,24 @@
+/*ungetchrÅiëÊ3î≈Åj*/
+
+#include<stdio.h>
+
+#define BUFSIZE 256
+
+extern char buffer[BUFSIZE];
+extern int buf_no;
+extern int front;
+extern int rear;
+
+/*1ï∂éöâüÇµñﬂÇ∑*/
+int ungetchr(int ch)
+{
+	if (buf_no >= BUFSIZE)
+		return EOF;
+	else {
+		buf_no++;
+		buffer[rear++] = ch;
+		if (rear == BUFSIZE)
+			rear = 0;
+		return ch;
+	}
+}
