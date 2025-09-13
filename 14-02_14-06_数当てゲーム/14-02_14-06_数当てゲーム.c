e840377ddd11e854603e3dfbbf13d7b218da61d6
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-09-13 23:42:57 +0900

    14-02_14-06_数当てゲーム
    ヘッダファイルとセット

---
+++ 14-02_14-06_数当てゲーム/14-02_14-06_数当てゲーム.c
@@ -0,0 +1,52 @@
+﻿/*数当てゲーム*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "display.h"
+
+int main(void)
+{
+	int no;
+	int ans;
+	const int max_stage = 10;
+	int remain = max_stage;
+
+	srand(time(NULL));
+	ans = rand() % 1000;
+
+	printf("０～９９９までの整数を当ててください\n\n");
+
+	do {
+		color(BRIGHT_WHITE);
+		printf("残り%d回、いくつかな :", remain);
+		scanf("%d", &no);
+		remain--;
+
+		if (no > ans) {
+			color(BRIGHT_CYAN);
+			printf("もっと小さいよ\n");
+		}
+		else if (no < ans) {
+			color(BRIGHT_GREEN);
+			printf("\aもっと大きいよ\n");
+		}
+	} while (no != ans && remain > 0);
+
+	if (no != ans) {
+		color(BRIGHT_RED);
+		printf("\a残念、正解は%dでした\n", ans);
+	}
+	else {
+		color(BRIGHT_CYAN);
+		printf("正解です\n");
+		printf("%d回で当たりましたね\n", max_stage - remain);
+	}
+
+	color(WHITE);
+
+	return 0;
+}
---
+++ 14-02_14-06_数当てゲーム/display.h
@@ -0,0 +1,33 @@
+/*画面制御ライブラリ（エスケープシーケンス）display.h*/
+
+#ifndef __DISPLAY
+#define __DISPLAY
+
+enum {
+	BLACK, 
+	BLUE, 
+	RED, 
+	GREEN, 
+	MAGENTA, 
+	CYAN, 
+	YELLOW, 
+	WHITE,
+	GRAY, 
+	BRIGHT_BLUE, 
+	BRIGHT_RED, 
+	BRIGHT_GREEN, 
+	BRIGHT_MAGENTA,
+	BRIGHT_CYAN, 
+	BRIGHT_YELLOW, 
+	BRIGHT_WHITE
+};
+
+void cls(void);
+
+void locate(int __x, int __y);
+
+void colorx(int __fg, int __bg);
+
+void color(int __col);
+
+#endif
